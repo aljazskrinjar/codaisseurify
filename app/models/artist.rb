fd3fde31a5779266302565e7f44ca3ba16581427
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-10-15 00:30:39 +0200

    Adding Validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_one :photo, dependent: :destroy
+
+  validates :name, presence: true
+  validates :age, numericality: { only_integer: true, :greater_than_or_equal_to => 3 }, presence: true
+  validates :bio, presence: true, length: { maximum: 500 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 
 artist1=Artist.create!(name: "Avseniki", age: 80, bio: "Avseniki are artist from SLovenia and ...", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUUtD06g0jzTBPKDK2hYsmeAQnBtUB3DHIBamkCvjkiFlkxhBHZw")
-artist2=Artist.create!(name: "Slaki", age: 80, bio: "Slaki are artist from SLovenia and ...", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUUtD06g0jzTBPKDK2hYsmeAQnBtUB3DHIBamkCvjkiFlkxhBHZw")
+artist2=Artist.create!(name: "Slaki", age: 70, bio: "Slaki are artist from SLovenia and ...", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUUtD06g0jzTBPKDK2hYsmeAQnBtUB3DHIBamkCvjkiFlkxhBHZw")
 
 
 song1 = Song.create!(name: "Cebelice",artist: artist1)
 song2 = Song.create!(name: "Na Golici",artist: artist1)
 song3 = Song.create!(name: "Ta sosedov Francl", artist: artist2)
+song4 = Song.create!(name: "Moja mukica", artist: artist2)
 
 
 photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/aljazcloud/image/upload/v1508014920/61092292_os1xc2.jpg", artist: artist1)
