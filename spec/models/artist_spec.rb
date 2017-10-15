1a413b80e7ddd8d6efe859d6226280782e4d0c43
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-10-15 13:07:58 +0200

    added some validation using rspec

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+   it "is invalid without a name" do
+
+     artist = Artist.new(name: "")
+     artist.valid?
+     expect(artist.errors).to have_key(:name)
+   end
+
+   it "This invalid:the bio is too long (>500 chars)" do
+
+     artist = Artist.new(bio: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibgadfgsdgfdsgsfdgfdsg")
+     artist.valid?
+     expect(artist.errors).to have_key(:bio)
+   end
+
+   #using shoulda-matchers
+   it { is_expected.to validate_presence_of(:age) }
+
+  end
 end
