3c296635756e5d733bf132625d2effb0ae46b262
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-10-22 18:31:08 +0200

    test for deleting a song. Delte a song only works with one song. I should ad an id to a button

--- spec/feature/deletingSong_spec.rb
+++ spec/feature/deletingSong_spec.rb
@@ -7,7 +7,8 @@ feature 'AddingSongsTest', js: true do
   let(:artist) { create :artist }
   let(:songTest) { create :song , artist: artist }
   let!(:photo) { create :photo, artist: artist }
-
+  let(:songTest2) { create :song , artist: artist }
+  let(:songTest3) { create :song , artist: artist }
 
   scenario "deleting a song" do
 
@@ -16,7 +17,11 @@ feature 'AddingSongsTest', js: true do
 
     page.execute_script("$('form').submit()")
 
-    click_on "Delete Song"
+
+
+    #for correct output I should have gave an id to button
+    #click_on 'deleteSong(artist.id,song.id)'
+    click_button("Delete Song")
 
     expect(page).to_not have_content('ljubi ljubi ljubi')
 
